ad3046b794e34d0228c54840201e056f25841cac
Shamim Reza <wpshamim@gmail.com>
2022-04-20 17:30:20 +0600

    cleaning code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-const elementShowHide = (show, id) => {
-    document.getElementById(id).style.display = show ? "block" : "none";
-};
-
+// get search value from input
 const getSearchInputValue = () => {
     const searchInputLg = document.getElementById("search-phone-lg");
     const searchInputMd = document.getElementById("search-phone-md");
@@ -14,11 +11,24 @@ const getSearchInputValue = () => {
     }
 };
 
+// set search input value in local storage
+const setLocalStorage = (keyword, key) => {
+    const keyStringify = JSON.stringify(keyword);
+    localStorage.setItem(key, keyStringify);
+};
+
+// element show hide condition
+const elementShowHide = (show, id) => {
+    document.getElementById(id).style.display = show ? "block" : "none";
+};
+
+// Handle Search keyword nad error handled
 const searchHandler = () => {
     const searchKeyword = getSearchInputValue();
     if (!searchKeyword) {
-        showToast("error-message", true);
+        showToast("error-handle", true, "Please Fill out Search Field");
     } else {
+        setLocalStorage(searchKeyword, "keyword");
         document.getElementById("search-keyword").innerText = searchKeyword;
         elementShowHide(true, "result-header");
         elementShowHide(false, "single-container");
@@ -28,22 +38,25 @@ const searchHandler = () => {
     }
 };
 
+// load search items form api
 const loadPhoneData = (searchKeyword) => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`)
         .then((res) => res.json())
         .then((data) => getSearchResult(data.data, data.status))
-        .catch((err) => console.log(err.message));
+        .catch((err) => showToast("error-handle", true, err.message));
 };
 
-// Display Default Devices
+// Display Default items
 const getStaticData = () => {
     fetch(`https://openapi.programming-hero.com/api/phones?search=g`)
         .then((res) => res.json())
-        .then((data) => getSearchResult(data.data, data.status));
+        .then((data) => getSearchResult(data.data, data.status))
+        .catch((err) => showToast("error-handle", true, err.message));
 };
-
+// default item displaying
 getStaticData();
 
+// Handled Search Header message
 const searchMessageChange = (status, message) => {
     const searchMessage = document.getElementById("search-message");
     status ? searchMessage.classList.add("border-success") : searchMessage.classList.remove("border-success");
@@ -65,14 +78,7 @@ const clearContainer = (id) => {
     return container;
 };
 
-const showMoreHandler = () => {
-    const searchKeyword = getSearchInputValue();
-    console.log(searchKeyword);
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`)
-        .then((res) => res.json())
-        .then((data) => getSearchResult(data.data, data.status, true));
-};
-
+// Get Search Result and Displaying 20 Items on UI
 const getSearchResult = (phones, status, isShowAll) => {
     if (!status) {
         clearContainer("phones-container");
@@ -80,7 +86,9 @@ const getSearchResult = (phones, status, isShowAll) => {
     } else {
         const phonesContainer = clearContainer("phones-container");
 
+        // Slice 20 Items, If Clicking Show Button Showing upto 100 Items
         const displayItems = phones.slice(0, !isShowAll ? 20 : 100);
+
         displayItems.forEach(({ brand, image, phone_name, slug }) => {
             searchMessageChange(status, `Your search returned ${phones.length} results. Only the most popular ${displayItems.length} devices shown.`);
             const card = createElement("col");
@@ -101,12 +109,30 @@ const getSearchResult = (phones, status, isShowAll) => {
     document.getElementById("show-more-btn").style.display = phones.length >= 20 ? "block" : "none";
 };
 
+// get search input value in local storage
+const getLocalStorage = (key) => {
+    const getKey = localStorage.getItem(key);
+    const keyword = JSON.parse(getKey);
+    return keyword;
+};
+
+// Show All Items Clicking by Show More Button
+const showMoreHandler = () => {
+    const searchKeyword = getLocalStorage("keyword");
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${searchKeyword}`)
+        .then((res) => res.json())
+        .then((data) => getSearchResult(data.data, data.status, true))
+        .catch((err) => showToast("error-handle", true, err.message));
+};
+
+// Handled Item Details Clicking by See Details Button on Each Item
 const handleSingleData = (slug) => {
     elementShowHide(true, "single-container");
     loadSingleData(slug);
     document.getElementById("single-container").scrollIntoView();
 };
 
+// Load Single Item from API Finding by slug
 const loadSingleData = (slug) => {
     const api = `https://openapi.programming-hero.com/api/phone/${slug}`;
     fetch(api)
@@ -114,7 +140,8 @@ const loadSingleData = (slug) => {
         .then((data) => getPhoneDetails(data.data));
 };
 
-const keySpecifications = (id, featureName, icon) => {
+// set Key Specifications right side of item details
+const setSpecifications = (id, featureName, icon) => {
     return `
         <div class="col d-flex mb-4 md-lg-5">
             <div class="feature-icon me-3">
@@ -127,15 +154,18 @@ const keySpecifications = (id, featureName, icon) => {
         </div>`;
 };
 
+// Get Single Items Details and showing on UI
 const getPhoneDetails = (data) => {
     const { slug, name, releaseDate, brand, image, mainFeatures } = data;
     const { storage, displaySize, chipSet, memory, sensors } = mainFeatures;
 
+    // others item error handled if other not exist in api
     const others = data?.others ? Object.entries(data?.others) : [["Error", "Others Feature Not Found"]];
 
+    // set sensors
     const displaySensors = sensors.map((sensor) => sensor);
 
-    // Others Feature
+    // set others Feature
     const otherFeatures = others.map(([featureName, feature]) => {
         return `
             <tr>
@@ -164,12 +194,12 @@ const getPhoneDetails = (data) => {
                     <h4 class="text-color py-3 ps-4 border fw-bolder">Key Specifications :</h4>
                 </div>
                 <div class="row row-cols-lg-2 row-cols-md-1 row-cols-sm-2 row-cols-1 mt-3 ps-3 py-3 border">
-                    ${keySpecifications(releaseDate ? releaseDate : "<span class='text-danger'>Release Date Not Announced</span>", "Release Date", "calendar-check")}
-                    ${keySpecifications(storage, "Storage", "sd-card")}
-                    ${keySpecifications(displaySize, "Display", "mobile-screen")}
-                    ${keySpecifications(memory, "Memory", "memory")}
-                    ${keySpecifications(chipSet ? chipSet : "<span class='text-danger'>Unspecified</span>", "ChipSet", "microchip")}
-                    ${keySpecifications(displaySensors.join(", "), "Sensors", "tower-broadcast")}
+                    ${setSpecifications(releaseDate ? releaseDate : "<span class='text-danger'>Release Date Not Announced</span>", "Release Date", "calendar-check")}
+                    ${setSpecifications(storage, "Storage", "sd-card")}
+                    ${setSpecifications(displaySize, "Display", "mobile-screen")}
+                    ${setSpecifications(memory, "Memory", "memory")}
+                    ${setSpecifications(chipSet ? chipSet : "<span class='text-danger'>Unspecified</span>", "ChipSet", "microchip")}
+                    ${setSpecifications(displaySensors.join(", "), "Sensors", "tower-broadcast")}
                 </div>
             </div>
         </div>
@@ -184,9 +214,11 @@ const getPhoneDetails = (data) => {
     singleContainer.appendChild(featuresDetails);
 };
 
-// Toast Showing
-const showToast = (id, isShowing) => {
-    let errorMessage = document.getElementById(id);
-    let errToast = new bootstrap.Toast(errorMessage);
+// Toast Showing if Search get Error
+const showToast = (id, isShowing, message) => {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerText = message;
+    let errorHandler = document.getElementById(id);
+    let errToast = new bootstrap.Toast(errorHandler);
     isShowing ? errToast.show() : errToast.hide();
 };
